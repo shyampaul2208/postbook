e18998402a1be565c9dd34c1a1247eda57e6fff6
shyam <paulshyam546@gmail.com>
2021-07-01 15:21:10 +0530

    loosing hope

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ const cors=require("cors");
 mongoose.connect("mongodb+srv://admin-shyam:shyampaul4041@cluster0.kodas.mongodb.net/instaDB",{useNewUrlParser:true,useUnifiedTopology:true});
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.use(
-    cors({
-      origin: "http://localhost:3000", // allow to server to accept request from different origin
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true // allow session cookie from browser to pass through
-    })
-  );
+// app.use(
+//     cors({
+//       origin: "http://localhost:3000", // allow to server to accept request from different origin
+//       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//       credentials: true // allow session cookie from browser to pass through
+//     })
+//   );
+
+app.use(cors());
   
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended:true,limit: '50mb'}))
