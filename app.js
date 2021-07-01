d758f6422c42cb318a597f34d0cec72b2f6e622b
shyam <paulshyam546@gmail.com>
2021-07-01 16:57:01 +0530

    testing

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ const cors=require("cors");
 mongoose.connect("mongodb+srv://admin-shyam:shyampaul4041@cluster0.kodas.mongodb.net/instaDB",{useNewUrlParser:true,useUnifiedTopology:true});
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
- app.use(
-     cors({
-       origin: "http://localhost:3000", // allow to server to accept request from different origin
-       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-       credentials: true // allow session cookie from browser to pass through
-     })
-   );
+//  app.use(
+//      cors({
+//        origin: "http://localhost:3000", // allow to server to accept request from different origin
+//        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//        credentials: true // allow session cookie from browser to pass through
+//      })
+//    );
+
+   app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "http://localhost:3000");
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
 
 
   
