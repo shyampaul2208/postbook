f0ee82107ba3c1482d06a461afdfbf9521602202
shyam <paulshyam546@gmail.com>
2021-07-01 15:28:32 +0530

    loosing hope 2

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 //     })
 //   );
 
-app.use(cors());
+app.use(cors(
+  {
+    "origin": "http://localhost:3000",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+    "credentials":true
+  }
+));
   
 app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({extended:true,limit: '50mb'}))
